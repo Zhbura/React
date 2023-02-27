05f30959c5ae837e9b176a5f33843894dc297a95
Zhbura <zhbura.nadezhda@yandex.ru>
2023-02-27 14:58:03 +0300

    Хранение chats в сторе.

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -2,45 +2,46 @@ import { Outlet, useParams } from "react-router-dom";
 import { Form } from "../Form/Form";
 import { ChatItem } from "./ChatItem";
 import { useSelector, useDispatch } from 'react-redux';
-import { addChat, deleteChat } from '../../store/chats/actions';
+import { initChatsTracking } from '../../store/chats/actions';
 import { nanoid } from 'nanoid';
 import { selectChats } from '../../store/chats/selectors';
 import './ChatList.css';
 import { getChatsRef, getChatsByIdRef, getMessagesRefByChatId, getMessageListRefByChatId } from "../../servises/firebase";
 import { useEffect, useState } from "react";
-import { onValue, set, remove, onChildRemoved, onChildAdded } from "firebase/database";
+import { onValue, set, onChildRemoved, onChildAdded } from "firebase/database";
+
+// 1 вариант взаимодействия с firebase через компонент. Всё работает, в базе данных отображается верно.
 
 export const ChatList = () => {
-    // const chats = useSelector(selectChats);
     const [chats, setChats] = useState([]);
 
     useEffect(() => {
-        const unsubscribe = onValue(getChatsRef, (snapshot) => {
-            const newChats = [];
-            snapshot.forEach((child) => {
-                newChats.push(child.val())
-            })
-            setChats(newChats);
-        })
+        const unsubscribe = onChildAdded(getChatsRef, (snapshot) => {
+            setChats((prevChats) => [...prevChats, snapshot.val()]);
+        });
 
         return unsubscribe;
-    }, [])
+    }, []);
 
-    // const dispatch = useDispatch();
+    useEffect(() => {
+        const unsubscribe = onChildRemoved(getChatsRef, (snapshot) => {
+            setChats((prevChats) =>
+                prevChats.filter(({ id }) => id !== snapshot.val()?.id)
+            );
+        });
+
+        return unsubscribe;
+    }, []);
 
     const handleAddChat = (newChatName) => {
         const newId = nanoid();
-        // dispatch(addChat(newId, newChatName));
 
         set(getChatsByIdRef(newId), { id: newId, name: newChatName });
         set(getMessagesRefByChatId(newId), { empty: true });
     }
 
     const handleDeleteChat = (id) => {
-        // dispatch(deleteChat(idToDelete));
 
-        // удаление чатов (2 способа):
-        // remove(getChatsByIdRef(id))
         set(getChatsByIdRef(id), null);
         set(getMessagesRefByChatId(id), null);
     }
@@ -59,3 +60,42 @@ export const ChatList = () => {
         </>
     )
 }
+
+// 2 вариант взаимодействия с firebase через store. '../../store/chats/actions'
+
+// export const ChatList = () => {
+//     const chats = useSelector(selectChats);
+
+//     const dispatch = useDispatch();
+
+//     const handleAddChat = (newChatName) => {
+//         const newId = nanoid();
+
+//         set(getChatsByIdRef(newId), { id: newId, name: newChatName });
+//         set(getMessagesRefByChatId(newId), { empty: true });
+//     }
+
+//     const handleDeleteChat = (id) => {
+//         set(getChatsByIdRef(id), null);
+//         set(getMessagesRefByChatId(id), null);
+//     }
+
+//     useEffect(() => {
+//         dispatch(initChatsTracking());
+//     }, [])
+
+//     return (
+//         <>
+//             <h1 className="heading_title">Чтобы начать общение - создай чат!</h1>
+//             <div className="chats_messages_block">
+//                 <div className="borderChatList">
+//                     {chats.map((chat) => (
+//                         <ChatItem key={chat.id} chat={chat} onDeleteChat={handleDeleteChat} />
+//                     ))}
+//                     <Form onSubmit={handleAddChat} buttonValue={'Создать чат'} />
+//                 </div>
+//                 <Outlet />
+//             </div>
+//         </>
+//     )
+// }
--- src/store/chats/actions.js
+++ src/store/chats/actions.js
@@ -1,3 +1,6 @@
+import { onChildAdded, onChildRemoved } from "firebase/database";
+import { getChatsRef } from "../../servises/firebase";
+
 export const ADD_CHAT = 'CHATS::ADD_CHAT';
 export const DELETE_CHAT = 'cHATS::DELETE_CHAT';
 
@@ -13,3 +16,17 @@ export const deleteChat = (id) => ({
     type: DELETE_CHAT,
     payload: id
 });
+
+// Миддлвар initChatsTracking, не работает, выдает ошибку (картинка в src),
+// он как будто копирует заново уже созданные чаты с одинаковыми id и от этого
+// выскакивает ошибка об уникальности.
+
+// export const initChatsTracking = () => (dispatch) => {
+//     onChildAdded(getChatsRef, (snapshot) => {
+//         dispatch(addChat(snapshot.val().id, snapshot.val().name));
+//     });
+
+//     onChildRemoved(getChatsRef, (snapshot) => {
+//         dispatch(deleteChat(snapshot.val().id));
+//     });
+// };
